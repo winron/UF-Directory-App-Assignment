bc98abd901ad864a3870107e103e73bf9f29f050
Winton Liu <33133071+winron@users.noreply.github.com>
2018-09-15 20:39:19 -0400

    Add files via upload

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,31 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
 
+
 /* Connect to your database */
-
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+mongoose.connect(config.db.uri);
+console.log('connection on');
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
-
-
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+var list = require('./listings.json').entries;
+for (var i = 0; i < list.length; i++) {
+  var newEntry = Listing(list[i]);
+  newEntry.save(function(err) {
+    if (err)
+    throw err;
+  });
+}
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
--- queries.js
+++ queries.js
@@ -1,28 +1,64 @@
 /* Fill out these functions using Mongoose queries*/
+var fs = require('fs'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
+
+    /* Connect to your database */
+    mongoose.connect(config.db.uri);
+    console.log('connection on');
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.find(
+     {name: 'Library West'},
+     function(err, listing) {
+       if(err)  throw err;
+       console.log(listing);
+     }
+   )
+
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+   Listing.findOneAndRemove(
+     {code: 'CABL'},
+     function(err,listing) {
+       if(err)  throw err;
+       console.log('CABL deleted');
+     }
+   )
 };
 var updatePhelpsLab = function() {
   /*
-    Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Laboratory's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
+   Listing.findOneAndUpdate(
+     {name: 'Phelps Laboratory'}, {address: '1953 Museum Rd, Gainesville, FL 32603'},
+     function(err,listing) {
+       if(err)  throw err;
+       console.log(listing);
+       console.log('address updated');
+     }
+   )
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+   Listing.find({}, function(err,listing) {
+     if(err) throw err;
+     console.log(listing);
+   });
 };
 
 findLibraryWest();
